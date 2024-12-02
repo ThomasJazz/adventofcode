f87fe0f1d322e24aa0a8f42a92c9541c626148d9
tjaszczult <ttjazztt@gmail.com>
2024-12-02 13:40:59 -0600

    Added tests

--- 2024/solutions/day02/daytwo_test.go
+++ 2024/solutions/day02/daytwo_test.go
@@ -16,12 +16,61 @@ func TestPartOne(t *testing.T) {
 }
 
 func TestPartTwo(t *testing.T) {
-	expected := 663
+	expected := 692
 	safeLevels := PartTwo()
 
 	println(safeLevels)
 
-	if safeLevels > expected {
+	if safeLevels != expected {
 		t.Fatalf(`Expected value: %d, Actual value: %d`, expected, safeLevels)
+	}
+}
+
+func TestCheckLine(t *testing.T) {
+	levels := []int{1, 2, 3, 4, 5}
+	if !CheckLine(levels) {
+		t.Fatalf(`Expected value: %t, Actual value: %t`, true, false)
+	}
+
+	levels = []int{5, 4, 3, 2, 1}
+	if !CheckLine(levels) {
+		t.Fatalf(`Expected value: %t, Actual value: %t`, true, false)
+	}
+
+	levels = []int{1, 2, 3, 5, 6}
+	if !CheckLine(levels) {
+		t.Fatalf(`Expected value: %t, Actual value: %t`, true, false)
+	}
+
+	levels = []int{1, 2, 3, 5, 4}
+	if CheckLine(levels) {
+		t.Fatalf(`Expected value: %t, Actual value: %t`, false, true)
+	}
+
+	levels = []int{1, 2, 3, 4, 8}
+	if CheckLine(levels) {
+		t.Fatalf(`Expected value: %t, Actual value: %t`, false, true)
+	}
+}
+
+func TestApplyDampener(t *testing.T) {
+	levels := []int{1, 2, 3, 4, 5}
+	if !ApplyDampener(levels) {
+		t.Fatalf(`Expected value: %t, Actual value: %t`, true, false)
+	}
+
+	levels = []int{5, 4, 3, 2, 1}
+	if !ApplyDampener(levels) {
+		t.Fatalf(`Expected value: %t, Actual value: %t`, true, false)
+	}
+
+	levels = []int{1, 1, 2, 3, 4}
+	if !ApplyDampener(levels) {
+		t.Fatalf(`Expected value: %t, Actual value: %t`, true, false)
+	}
+
+	levels = []int{1, 2, 3, 1, 5}
+	if !ApplyDampener(levels) {
+		t.Fatalf(`Expected value: %t, Actual value: %t`, true, false)
 	}
 }
