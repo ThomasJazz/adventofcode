f8a9549a4ac718657ca8e213eb0eae5619d4609d
Thomas Jaszczult <ttjazztt@gmail.com>
2025-12-03 09:46:10 -0800

    Day 3 pt 2

--- 2025/solutions/day3/day3.go
+++ 2025/solutions/day3/day3.go
@@ -18,26 +18,79 @@ func PartOne() int {
 	var maxJoltages []int
 
 	for _, line := range lines {
-		maxLeft, maxRight := -1, -1
-
-		for i := 0; i < len(line)-1; i++ {
-			leftNum, _ := strconv.Atoi(string(line[i]))
-			rightNum, _ := strconv.Atoi(string(line[i+1]))
-
-			if leftNum > maxLeft {
-				maxLeft = leftNum
-				maxRight = -1
-			}
-
-			if rightNum > maxRight {
-				maxRight = rightNum
-			}
-		}
-		concatted, _ := strconv.Atoi(strconv.Itoa(maxLeft) + strconv.Itoa(maxRight))
-		maxJoltages = append(maxJoltages, concatted)
+		maxJoltages = append(maxJoltages, GetJoltagePtOne(line))
 	}
 
 	return sumMaxJoltages(maxJoltages)
+}
+
+func GetJoltagePtOne(line string) int {
+	maxLeft, maxRight := -1, -1
+
+	for i := 0; i < len(line)-1; i++ {
+		leftNum, _ := strconv.Atoi(string(line[i]))
+		rightNum, _ := strconv.Atoi(string(line[i+1]))
+
+		if leftNum > maxLeft {
+			maxLeft = leftNum
+			maxRight = -1
+		}
+
+		if rightNum > maxRight {
+			maxRight = rightNum
+		}
+	}
+	concatted, _ := strconv.Atoi(strconv.Itoa(maxLeft) + strconv.Itoa(maxRight))
+	return concatted
+}
+
+func PartTwo() int64 {
+	fmt.Println("Day 3 solution pt 2")
+	lines := lib.ReadInput(filename)
+	var maxJoltages []int64
+
+	for _, line := range lines {
+		maxJoltages = append(maxJoltages, GetJoltagePtTwo(line))
+	}
+
+	return sumMaxJoltages64(maxJoltages)
+}
+
+func GetJoltagePtTwo(line string) int64 {
+	var joltage string
+
+	k := 0
+	lineLength := len(line)
+	for i := 0; i < 12; i++ {
+		currentMax := -1
+		currentMaxIndex := 0
+		maxScanIndex := lineLength - (11 - i)
+
+		for k < maxScanIndex {
+			current, _ := strconv.Atoi(string(line[k]))
+
+			if current > currentMax {
+				currentMax = current
+				currentMaxIndex = k
+			}
+			k++
+		}
+		// Restart search from where we found last max number
+		k = currentMaxIndex + 1
+
+		joltage = joltage + strconv.Itoa(currentMax)
+	}
+
+	i64, _ := strconv.ParseInt(joltage, 10, 64)
+	return i64
+}
+
+func sumMaxJoltages64(joltages []int64) int64 {
+	sum := int64(0)
+	for _, val := range joltages {
+		sum += val
+	}
+	return sum
 }
 
 func sumMaxJoltages(joltages []int) int {
@@ -46,9 +99,4 @@ func sumMaxJoltages(joltages []int) int {
 		sum += val
 	}
 	return sum
-}
-
-func PartTwo() {
-	fmt.Println("Day 3 solution pt 2")
-	// lines := lib.ReadInput(filename)
 }
--- 2025/solutions/day3/day3_test.go
+++ 2025/solutions/day3/day3_test.go
@@ -2,6 +2,8 @@ package day3
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestPartOne(t *testing.T) {
@@ -11,6 +13,39 @@ func TestPartOne(t *testing.T) {
 }
 
 func TestPartTwo(t *testing.T) {
+	assert := assert.New(t)
 	// Add tests here
-	PartTwo()
+	testInput := "811111111111119"
+	var expected int64 = 811111111119
+
+	result := GetJoltagePtTwo(testInput)
+
+	assert.Equal(expected, result, "Expected should match result")
+}
+
+func TestPartTwoAgain(t *testing.T) {
+	assert := assert.New(t)
+	// Add tests here
+	testInput := "234234234234278"
+	var expected int64 = 434234234278
+
+	result := GetJoltagePtTwo(testInput)
+
+	assert.Equal(expected, result, "Expected should match result")
+}
+
+func TestPartTwoAgainAgain(t *testing.T) {
+	assert := assert.New(t)
+	// Add tests here
+	testInput := "818181911112111"
+	var expected int64 = 888911112111
+
+	result := GetJoltagePtTwo(testInput)
+
+	assert.Equal(expected, result, "Expected should match result")
+}
+
+func TestPartTwoFull(t *testing.T) {
+	result := PartTwo()
+	println(result)
 }
